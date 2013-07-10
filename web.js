1caabcfeed2f7b48409b3685736e2e0711109446
damirpasa <damirpasa@gmail.com>
2013-07-10 17:50:31 +0200

    new web.js

--- web.js
+++ web.js
@@ -1,12 +1,9 @@
-var express = require('express');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
+var content = fs.readFileSync('index.html');
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+console.log(content);
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+buf = new Buffer(content);
+
+console.log(buf.toString());
